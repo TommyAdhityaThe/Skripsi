e7345f51f1d5cbfc71eefba1b5fb97cefdb94faf
TommyAdhityaThe <toms.warior@gmail.com>
2016-05-02 03:51:23 +0700

    tambah index.php buat ditanyain dan statistik data

---
+++ doc/Dokumen Penting/index.php
@@ -0,0 +1,25 @@
+<?php 
+	$b = 501;
+	$a = "0x500ysdasdaa";
+	$c = $a+$b;
+	echo $c . "<br>";
+	//hasil 1781
+
+	$b = 501;
+	$a = "0x500asdasdaa";
+	$c = $a+$b;
+	echo $c . "<br>";
+	//hasil 20991
+
+	$b = 501;
+	$a = "500daa";
+	$c = $a+$b;
+	echo $c . "<br>";
+	//hasil 11001
+
+	$b = 501;
+	$a = "a500daa";
+	$c = $a+$b;
+	echo $c . "<br>";
+	//hasil 501
+ ?>
